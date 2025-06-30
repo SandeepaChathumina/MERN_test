58b7fcf59cde11657b19d4ab34252878eef83760
Sandeepa-Karunanayake <sandeepachathuminaofficial@gmail.com>
2025-07-01 01:31:20 +0530

    dbconnection

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("Hello, World!");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Middleware
+app.use("/", (req, res, next) => {
+    res.send("It is working");
+});
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://sandy:Ehqk1YWBdWBGDKmf@cluster0.bctfbpk.mongodb.net/")
+    .then(() => {
+        console.log("Connected to MongoDB");
+
+        // Start the server only after successful connection
+        app.listen(5000, () => {
+            console.log("Server started on port 5000");
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error:", err);
+    });
+
+    
+
+
+
+//Ehqk1YWBdWBGDKmf
