23759d28c75b2c03696ef50dbeb9495b5878b1c7
Sandeepa-Karunanayake <sandeepachathuminaofficial@gmail.com>
2025-07-02 18:26:52 +0530

    frontend setup

--- frontend/src/components/UserDetails/Users.js
+++ frontend/src/components/UserDetails/Users.js
@@ -1,22 +1,19 @@
-import React, { useEffect, useState} from 'react';
-//import React from 'react'
+import React, { useEffect } from 'react'
 import Nav from '../Nav/Nav'
 import axios from "axios";
 
-const URL = 'http://localhost:5000/users'
+const URL = "http://localhost:5000/users";
 
-const fetchHandler = async () => {
-  return await axios.get(URL).then((res) => res.data); 
+const fetchHandler  = async () => {
+  return await axios.get(URL).then((res) => res.data);
 }
-
 function Users() {
 
   const [users, setUsers] = useState();
   useEffect(() => {
     fetchHandler().then((data) => setUsers(data.users));
-    }, []);
-
-
+  },[])
+  
   return (
     <div>
       <Nav />
